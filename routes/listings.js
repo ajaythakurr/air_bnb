657d01c425666950e0fa6b8d998122e9b7875412
Ajay Thakur <ajay.0128th@gmail.com>
2025-05-08 12:10:52 +0530

    used express router to amke app.js code shorter and trnsferred all the routes of listings and reviews to another folder routes

---
+++ routes/listings.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const wrapAsync = require("../utels/wrapAsync");
+const {listingSchema} = require("../schema");
+const ExpressError = require("../utels/ExpressErrors");
+const Listing = require("../models/listing");
+const router = express.Router();
+
+
+const validateListing = (req,res,next)=>{
+    let{error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg= error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
+
+//index route
+router.get("/",wrapAsync(async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+})); 
+
+//new Create route
+router.get("/newlist",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+router.post("/",
+    validateListing,
+    wrapAsync(async (req,res,next)=>{
+        const listing =new Listing(req.body);
+        await listing.save();
+        res.redirect("/listings");  
+    }
+));
+
+//Show route
+router.get("/:id", wrapAsync(async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs",{listing});
+}))
+
+//edit route
+router.get("/:id/edit",wrapAsync(async(req,res)=>{
+    let {id}= req.params;
+    let listing =await Listing.findById(id);
+    res.render("listings/edit",{listing});
+}));
+//update route
+router.put("/:id",wrapAsync(async (req,res)=>{
+    let {id}= req.params;
+    const {title,description,price,location,country}=req.body;
+    await Listing.findByIdAndUpdate(id,{title:title,description:description,price:price,location:location,country:country});
+    res.redirect("/listings");
+}));
+
+//delete route
+router.delete("/:id",wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    let deletedListing= await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+}));
+
+module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const wrapAsync = require("../utels/wrapAsync");
+const {reviewSchema} = require("../schema");
+const ExpressError = require("../utels/ExpressErrors");
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+const router = express.Router({mergeParams: true});
+
+
+const validatateReview = (req,res,next)=>{
+    let{error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg= error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
+
+//post review route
+router.post("/",
+    validatateReview, 
+    wrapAsync (async (req,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body);
+    listing.reviews.push(newReview);
+    
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+
+}));
+//delete review route
+router.delete("/:reviewId",wrapAsync(async (req,res)=>{
+    let{id,reviewId} = req.params;
+
+    let res1 = await Listing.findByIdAndUpdate(id, {$pull: {reviews:reviewId}});
+    let res2 = await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+
+}));
+
+module.exports = router;
